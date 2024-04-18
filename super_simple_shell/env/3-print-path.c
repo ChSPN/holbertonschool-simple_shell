b96b95ac23b5843dde27c9db1f99e33c83dc46a4
ChSPN <charlene.scomparin@gmail.com>
2024-04-18 08:01:08 +0200

    function that prints each directory contained in the the environment variable PATH (betty style)

--- super_simple_shell/env/3-print-path.c
+++ super_simple_shell/env/3-print-path.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* printpath -Prints the path from the root node to a given node
+* @path: Pointer to the node to which the path is printed
+*
+* Description: recursively prints the path from root to given node
+* Return: 0
+*/
+
 void printpath(void)
 {
 	char *path = getenv("PATH");
