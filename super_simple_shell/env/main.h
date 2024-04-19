8150e825e3a087fcb3ff5748ac51288dbedbab7c
ChSPN <charlene.scomparin@gmail.com>
2024-04-19 16:14:00 +0200

    Prototypes etc

--- super_simple_shell/env/main.h
+++ super_simple_shell/env/main.h
@@ -1,8 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef MAIN_H
-#define MAIN_H
+
+/**
+* struct path - Struct PATH variable
+* @value: The value of the directory path
+* @next: Pointer to the next node in the linked list
+*/
+typedef struct path
+{
+	char *value;
+	struct path *next;
+} path_t;
 
 /*PROTOTYPES*/
 /*0-printenv-environ.c*/
@@ -30,16 +42,5 @@ int _unsetenv(const char *name);
 /*AUTRES*/
 
 extern char **environ;
-
-/**
-* struct path - Struct PATH variable
-* @value: The value of the directory path
-* @next: Pointer to the next node in the linked list
-*/
-typedef struct path
-{
-	char *value;
-	struct path *next;
-} path_t;
 
 #endif
