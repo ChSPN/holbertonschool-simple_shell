20e0f62cf7655f0001446e38ee79c776c2e6d2ac
ninotna <antonin@dev87.fr>
2024-04-22 15:30:57 +0200

    sep folder for shell_1.0

---
+++ shell_1.0/shell.h
@@ -0,0 +1,28 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h> /* Include for struct stat and stat() */
+
+#define MAX_COMMAND_LENGTH 100
+#define PROMPT "$cisfun "
+#define MAX_ARGS 10
+
+/* shell.h */
+void print_env(void);
+void exit_shell(char **args);
+
+/* Function Prototypes */
+int read_line(char *buffer, int size);
+char **parse_command(char *command);
+void execute_command(char **args);
+char *find_command_in_path(char *command);
+
+#endif /* SHELL_H */
